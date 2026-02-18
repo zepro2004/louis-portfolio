a3883aae5f533c14f01f2e006b119c4dcb448a08
Louis Bertrand Ntwali <lbertrand01@proton.me>
2026-02-18 15:08:36 -0500

    Update projects data and assets
    - Add NeonFetch project entry with new image and metadata
    - Remove legacy converter/validator projects and their images (Palindrome Checker, Decimal Converter, Telephone Validator, Roman Converter)

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,18 @@
+import neonfetchImage from "../assets/images/projects/neonfetch.png";
 import notesAppImage from "../assets/images/projects/NotesApp.png";
 import eventsWebsiteImage from "../assets/images/projects/EventsWebsite.png";
-import palindromeCheckerImage from "../assets/images/projects/PalindromeChecker.png";
-import decimalConverterImage from "../assets/images/projects/DecimalConverter.png";
-import telephoneValidatorImage from "../assets/images/projects/TelephoneValidator.png";
-import romanConverterImage from "../assets/images/projects/RomanConverter.png";
 
 const projects = [
+  {
+    title:"NeonFetch",
+    description:
+      "A modern desktop downloader built with Tauri, React, and yt-dlp. Download videos from YouTube and 1000+ other platforms with a beautiful, easy-to-use interface.",
+    date: "2026-01-31",
+    image: neonfetchImage,
+    source: "https://github.com/zepro2004/yt-dlp-gui",
+    caption: "Fig. NeonFetch",
+    technologies: "React, Rust, Tauri",
+  },
   {
     title: "Notes App",
     description:
@@ -26,46 +33,6 @@ const projects = [
     source: "https://github.com/zepro2004/Event-Attendees-Management-Website",
     caption: "Fig. Events Management Website",
     technologies: "JavaScript, PHP, SQL, HTML, CSS",
-  },
-
-  {
-    title: "Palindrome Checker",
-    description:
-      "Checks if a word or sentence is a palindrome. It ignores special characters and spaces.",
-    date: "2024-11-25",
-    image: palindromeCheckerImage,
-    source: "https://jsfiddle.net/bcbzepro/bhLc2a3p/1/",
-    caption: "Fig. palindrome checker",
-    technologies: "JavaScript, HTML, CSS",
-  },
-  {
-    title: "Decimal Converter",
-    description: "Converts numbers from decimal to binary.",
-    date: "2024-12-13",
-    image: decimalConverterImage,
-    source: "https://jsfiddle.net/bcbzepro/o53hcbLn/1/",
-    caption: "Fig. Decimal to binary converter",
-    technologies: "JavaScript, HTML, CSS",
-  },
-  {
-    title: "Telephone Validator",
-    description:
-      "Checks that the telephone number is in the correct format. It follows the north american numbering format.",
-    date: "2025-01-15",
-    image: telephoneValidatorImage,
-    source: "https://jsfiddle.net/bcbzepro/9suLda7r/1/",
-    caption: "Fig. Telephone number validator",
-    technologies: "JavaScript, HTML, CSS",
-  },
-  {
-    title: "Roman Numeral Converter",
-    description:
-      "Converts numbers to Roman numerals. You input any number from 1 to 3999 and it will convert to a roman numeral.",
-    date: "2024-09-25",
-    image: romanConverterImage,
-    source: "https://jsfiddle.net/bcbzepro/vm39fja0/1/",
-    caption: "Fig. Roman numeral converter",
-    technologies: "JavaScript, HTML, CSS",
   },
 ];
 
